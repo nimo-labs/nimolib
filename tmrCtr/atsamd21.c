356b50d73a5927c54ce3f69eb4d69d1a8d4bdb7d
Nick <nick@nimo.uk>
2020-11-28 17:24:07 +0000

    Fixed various bugs and changed based timer to TC4 to allow 32bit chaining with TC5

--- tmrCtr/atsamd21.c
+++ tmrCtr/atsamd21.c
@@ -20,28 +20,29 @@
 
 void tmrCtrInit(unsigned int tickInterval) /* tick in tickInterval sec*/
 {
+    PM->APBCMASK.reg |= PM_APBCMASK_TC4 | PM_APBCMASK_TC5;
 
-    PM->APBCMASK.reg |= PM_APBCMASK_TC3;
+    GCLK->CLKCTRL.reg =  GCLK_CLKCTRL_ID_TC4_TC5 |
+                         GCLK_CLKCTRL_CLKEN | GCLK_CLKCTRL_GEN(PERHIP_CLK_GEN);
+    while (GCLK->STATUS.bit.SYNCBUSY);               // Wait for synchronization
 
-    GCLK->CLKCTRL.reg = GCLK_CLKCTRL_ID(TC3_GCLK_ID) |
-                        GCLK_CLKCTRL_CLKEN | GCLK_CLKCTRL_GEN(PERHIP_CLK_GEN);
-
-    TC3->COUNT32.CTRLA.reg = 0;
-    TC3->COUNT32.CTRLA.reg |= TC_CTRLA_PRESCSYNC_GCLK | TC_CTRLA_RUNSTDBY | TC_CTRLA_PRESCALER_DIV1024 |
-                              TC_CTRLA_WAVEGEN_NFRQ | TC_CTRLA_MODE_COUNT32 | TC_CTRLA_ENABLE;
-    TC3->COUNT32.CC[0].reg = tickInterval * 46875;
-    TC3->COUNT32.INTENSET.reg |= TC_INTENCLR_MC0;
-    NVIC_EnableIRQ(TC3_IRQn); /*Enable SERCOM interrupt*/
+    TC4->COUNT32.CTRLA.reg = 0;
+    TC4->COUNT32.CTRLA.reg |= TC_CTRLA_PRESCSYNC_GCLK | TC_CTRLA_RUNSTDBY | TC_CTRLA_PRESCALER_DIV1024 |
+                              TC_CTRLA_WAVEGEN_MFRQ | TC_CTRLA_MODE_COUNT32 | TC_CTRLA_ENABLE;
+    while (TC4->COUNT32.STATUS.bit.SYNCBUSY);        // Wait for synchronization
+    TC4->COUNT32.CC[0].reg = tickInterval * (UP_CLK / 1024);
+    TC4->COUNT32.INTENSET.reg |= TC_INTENSET_MC0;
+    NVIC_EnableIRQ(TC4_IRQn); /*Enable TC interrupt*/
 }
 
 
 /*ext_usart handler*/
 
-void irq_handler_tc3(void)
+void irq_handler_tc4(void)
 {
-    if(TC3->COUNT32.INTFLAG.reg & TC_INTFLAG_MC0)
+    if(TC4->COUNT32.INTFLAG.reg & TC_INTFLAG_MC0)
     {
-        printf("TC INT\r\n");
+        printf("TC INT 0x%.2X\r\n", TC4->COUNT32.INTFLAG.reg);
         TC3->COUNT32.INTFLAG.reg |= TC_INTFLAG_MC0;
     }
 }
