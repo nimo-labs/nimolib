5d32cb7f3008d05c27cc58399a925f56f0ed2505
Nick <nick@nimo.uk>
2021-02-16 00:46:35 +0000

    Fixed HID descriptor

--- usbHid/m032/descriptors.c
+++ usbHid/m032/descriptors.c
@@ -15,22 +15,24 @@
 /*!<USB HID Report Descriptor */
 uint8_t HID_DeviceReportDescriptor[] =
 {
-    0x06, 0x00, 0xff, // Usage Page (Vendor Defined 0xFF00)
-    0x09, 0x01,		  // Usage (0x01)
-    0xa1, 0x01,		  // Collection (Application)
-    0x15, 0x00,		  //   Logical Minimum (0)
-    0x26, 0xff, 0x00, //   Logical Maximum (255)
-    0x75, 0x08,		  //   Report Size (8)
-    0x95, 0x40,		  //   Report Count (64)
-    0x09, 0x01,		  //   Usage (0x01)
-    0x81, 0x02,		  //   Input (Data,Var,Abs,No Wrap,Linear,Preferred State,No Null Position)
-    0x95, 0x40,		  //   Report Count (64)
-    0x09, 0x01,		  //   Usage (0x01)
-    0x91, 0x02,		  //   Output (Data,Var,Abs,No Wrap,Linear,Preferred State,No Null Position,Non-volatile)
-    0x95, 0x01,		  //   Report Count (1)
-    0x09, 0x01,		  //   Usage (0x01)
-    0xb1, 0x02,		  //   Feature (Data,Var,Abs,No Wrap,Linear,Preferred State,No Null Position,Non-volatile)
-    0xc0,			  // End Collection
+    0x06, 0x00, 0xFF,   // Usage Page = 0xFF00 (Vendor Defined Page 1)
+    0x09, 0x01,         // Usage (Vendor Usage 1)
+    0xA1, 0x01,         // Collection (Application)
+    0x19, 0x01,         // Usage Minimum
+    0x29, 0x40,         // Usage Maximum //64 input usages total (0x01 to 0x40)
+    0x15, 0x00,         // Logical Minimum (data bytes in the report may have minimum value = 0x00)
+    0x26, 0xFF, 0x00,   // Logical Maximum (data bytes in the report may have maximum value = 0x00FF = unsigned 255)
+    0x75, 0x08,         // Report Size: 8-bit field size
+    0x95, 0x40,         // Report Count: Make sixty-four 8-bit fields (the next time the parser hits
+    // an "Input", "Output", or "Feature" item)
+    0x81, 0x00,         // Input (Data, Array, Abs): Instantiates input packet fields based on the
+    // above report size, count, logical min/max, and usage.
+    0x19, 0x01,         // Usage Minimum
+    0x29, 0x40,         // Usage Maximum //64 output usages total (0x01 to 0x40)
+    0x91, 0x00,         // Output (Data, Array, Abs): Instantiates output packet fields. Uses same
+    // report size and count as "Input" fields, since nothing new/different was
+    // specified to the parser since the "Input" item.
+    0xC0                // End Collection
 };
 
 
