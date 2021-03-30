3eebae7f7f6ebe30b4dce531fe605ab76568262c
Nick <nick@nimo.uk>
2021-03-30 16:29:01 +0100

    Basic working XN297 driver implemented

--- xn297l/xn297l.c
+++ xn297l/xn297l.c
@@ -44,12 +44,21 @@
 
 #include "xn297l.h"
 
-static uint8_t xn297lReadReg(uint8_t reg);
+//static
+uint8_t xn297lReadReg(uint8_t reg);
 static uint8_t xn297lWriteReg(uint8_t reg, uint8_t value);
+
+
+static const uint8_t xn297Preamble[] =
+{
+    0b01110001,
+    0b00001111,
+    0b01010101
+};
 
 void xn297lInit(void)
 {
-    printf("at86rf23xInit()...");
+    printf("xn297lInit()...");
 
     /*Set CS pin as output and high*/
     GPIO_PIN_DIR(XN297L_SS_PORT, XN297L_SS_PIN, GPIO_DIR_OUT);
@@ -64,8 +73,15 @@ void xn297lInit(void)
 
 
     /*Set address len to 3 bytes */
-    xn297lWriteReg(XN297L_REG_SETUP_AW, 0x01);
+    // xn297lWriteReg(XN297L_REG_SETUP_AW, 0x01);
 
+    /*Set recieve payload to 64 bytes*/
+    xn297lWriteReg(XN297L_REG_RX_PW_P0, 64);
+
+    /*Power up Radio*/
+    uint8_t configReg=0x00;
+    configReg |= (1 << 1 /* PWR_UP*/);
+    xn297lWriteReg(XN297L_REG_CONFIG, configReg);
 
     printf("Config reg: 0x%.2X\r\n", xn297lReadReg(XN297L_REG_CONFIG));
     printf("Status reg: 0x%.2X\r\n", xn297lReadReg(XN297L_REG_STATUS));
@@ -77,7 +93,7 @@ void xn297lEnRxMode(void)
 {
     uint8_t configReg=xn297lReadReg(XN297L_REG_CONFIG);
 
-    configReg |= (1 << 7 /*EN_PM*/) | (1 << 3 /*PWR_UP*/);
+    configReg |= (1 << 7 /*EN_PM*/) | (1 << 1 /*PWR_UP*/) | (1 << 0 /* PRIM_RX*/);
     xn297lWriteReg(XN297L_REG_CONFIG, configReg);
 }
 
@@ -99,8 +115,17 @@ void xn297lSetDataRate(uint8_t dataRate)
 {
     uint8_t rfSetupReg=xn297lReadReg(XN297L_REG_RFSETUP);
 
-    rfSetupReg &= ~(0x03 << 6); /*Clear channel*/
-    rfSetupReg |= (dataRate << 6); /*Set channel*/
+    rfSetupReg &= ~(0x03 << 6); /*Clear datarate*/
+    rfSetupReg |= (dataRate << 6); /*Set datarate*/
+    xn297lWriteReg(XN297L_REG_RFSETUP, rfSetupReg);
+}
+
+void xn297lSetTxPower(uint8_t power)
+{
+    uint8_t rfSetupReg=xn297lReadReg(XN297L_REG_RFSETUP);
+
+    rfSetupReg &= ~(0x3F << 0); /*Clear Tx power*/
+    rfSetupReg |= (0x2A << 0); /*Set TX Power*/
     xn297lWriteReg(XN297L_REG_RFSETUP, rfSetupReg);
 }
 
@@ -127,4 +152,77 @@ uint8_t xn297lReadReg(uint8_t reg)
     GPIO_PIN_OUT(XN297L_SS_PORT, XN297L_SS_PIN, GPIO_OUT_HIGH);
 
     return regData;
+}
+
+uint8_t xn297lTransmitData(uint8_t *data, uint8_t len)
+{
+    if(!(xn297lReadReg(XN297L_REG_STATUS) & (1 << 0))) /*Check for TX FIFO Full*/
+    {
+        /*Enable TX Mode*/
+        uint8_t configReg=xn297lReadReg(XN297L_REG_CONFIG);
+        configReg &= ~(1 << 0 /* PRIM_RX*/);
+        xn297lWriteReg(XN297L_REG_CONFIG, configReg);
+
+        GPIO_PIN_OUT(XN297L_SS_PORT, XN297L_SS_PIN, GPIO_OUT_LOW);
+        spiTxByte(XN297L_SPI_CHAN, XN297L_CMD_TX_PAYLOAD);
+
+        for(uint8_t i=0; i < len; i++)
+            spiTxByte(XN297L_SPI_CHAN, data[i]);
+        GPIO_PIN_OUT(XN297L_SS_PORT, XN297L_SS_PIN, GPIO_OUT_HIGH);
+
+        delayMs(25);
+
+        uint8_t statusReg=xn297lReadReg(XN297L_REG_STATUS);
+        if(statusReg & (1 << 5))
+        {
+            // TX_DATA_SENT
+            printf("data sent\r\n");
+            // clear TX_DATA_SENT (write 1 to clear)
+            statusReg |= (1 << 5);
+            xn297lWriteReg(XN297L_REG_STATUS, statusReg);
+
+        }
+        else
+        {
+            printf("data not sent\r\n");;
+        }
+
+        /*Enable RX mode*/
+        configReg=xn297lReadReg(XN297L_REG_CONFIG);
+        configReg |= (1 << 0 /* PRIM_RX*/);
+        xn297lWriteReg(XN297L_REG_CONFIG, configReg);
+    }
+    else
+    {
+        printf("Transmit: FIFO full\r\n");
+        return -1;
+    }
+    return 0;
+}
+
+int8_t xn297lReceiveData(uint8_t *data, uint8_t bufMaxLen)
+{
+    if(!(xn297lReadReg(XN297L_REG_STATUS) & 0x40)) /*Check for TX FIFO Full*/
+        return 0; /*No data avilable */
+    else
+    {
+        uint8_t dataLen=xn297lReadReg(XN297L_REG_RX_PW_P0);
+        if(dataLen > bufMaxLen)
+            return -1;
+        GPIO_PIN_OUT(XN297L_SS_PORT, XN297L_SS_PIN, GPIO_OUT_LOW);
+        spiTxByte(XN297L_SPI_CHAN, XN297L_CMD_RX_PAYLOAD);
+
+        for(int i=0; i<dataLen; i++)
+        {
+            data[i] = spiRxByte(XN297L_SPI_CHAN);
+        }
+
+        GPIO_PIN_OUT(XN297L_SS_PORT, XN297L_SS_PIN, GPIO_OUT_HIGH);
+
+        uint8_t statusReg=xn297lReadReg(XN297L_REG_STATUS);
+        // clear TX_DATA_SENT (write 1 to clear)
+        statusReg |= (1 << 6);
+        xn297lWriteReg(XN297L_REG_STATUS, statusReg);
+        return dataLen;
+    }
 }
