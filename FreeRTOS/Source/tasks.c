70a45985310e76a9b9161e5d789177744bd66402
Nick Dark <nick@nimo.uk>
2024-02-27 23:56:21 +0000

    Linting

--- FreeRTOS/Source/tasks.c
+++ FreeRTOS/Source/tasks.c
@@ -845,7 +845,7 @@ static void prvInitialiseNewTask( TaskFunction_t pxTaskCode,
 #if ( tskSET_NEW_STACKS_TO_KNOWN_VALUE == 1 )
     {
         /* Fill the stack with a known value to assist debugging. */
-        ( void ) memset( pxNewTCB->pxStack, ( int ) tskSTACK_FILL_BYTE, ( size_t ) ulStackDepth * sizeof( StackType_t ) );
+        ( void ) memset_s( pxNewTCB->pxStack, sizeof(pxNewTCB->pxStack), ( int ) tskSTACK_FILL_BYTE, ( size_t ) ulStackDepth * sizeof( StackType_t ) );
     }
 #endif /* tskSET_NEW_STACKS_TO_KNOWN_VALUE */
 
@@ -856,6 +856,7 @@ static void prvInitialiseNewTask( TaskFunction_t pxTaskCode,
 #if ( portSTACK_GROWTH < 0 )
     {
         pxTopOfStack = &( pxNewTCB->pxStack[ ulStackDepth - ( uint32_t ) 1 ] );
+        //NOLINTNEXTLINE(clang-diagnostic-pointer-to-int-cast) See next line for justification
         pxTopOfStack = ( StackType_t * ) ( ( ( portPOINTER_SIZE_TYPE ) pxTopOfStack ) & ( ~( ( portPOINTER_SIZE_TYPE ) portBYTE_ALIGNMENT_MASK ) ) ); /*lint !e923 !e9033 !e9078 MISRA exception.  Avoiding casts between pointers and integers is not practical.  Size differences accounted for using portPOINTER_SIZE_TYPE type.  Checked by assert(). */
 
         /* Check the alignment of the calculated top of stack is correct. */
